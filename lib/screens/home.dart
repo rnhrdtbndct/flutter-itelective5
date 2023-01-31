b0890ba751803a3f085ef6ac99200788b713bcff
reinhardt <rhvbenedicto@gmail.com>
2023-01-31 12:56:48 +0800

    edits for PA #4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,17 @@
 import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:itelective5/card.dart';
+import 'package:itelective5/navbar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,39 +22,43 @@ class HomeScreen extends StatelessWidget {
         child: Wrap(
           children: [
             Container(
-              width: MediaQuery.of(context).size.width * 1/3,
+              width: MediaQuery.of(context).size.width * 1 / 3,
               height: MediaQuery.of(context).size.height,
               alignment: Alignment.center,
               color: Colors.red,
-              child: Image.asset('assets/images/kanagawa.png', fit: BoxFit.cover, width: 150, height: 150,)
+              child: SidebarXExampleApp(),
             ),
             Container(
-              width: MediaQuery.of(context).size.width * 1/3,
-              height: MediaQuery.of(context).size.height,
-              alignment: Alignment.center,
-              color: Colors.green,
-              child: Image.asset('assets/images/kanagawa.png', fit: BoxFit.cover, width: 150, height: 150,)
-            ),
+                width: MediaQuery.of(context).size.width * 1 / 3,
+                height: MediaQuery.of(context).size.height,
+                alignment: Alignment.center,
+                color: Colors.green,
+                child: Image.asset(
+                  'assets/images/kanagawa.png',
+                  fit: BoxFit.cover,
+                  width: 150,
+                  height: 150,
+                )),
             Container(
-              width: MediaQuery.of(context).size.width * 1/3,
+              width: MediaQuery.of(context).size.width * 1 / 3,
               height: MediaQuery.of(context).size.height,
               alignment: Alignment.center,
               child: Wrap(
                 children: [
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 1/2,
+                    height: MediaQuery.of(context).size.height * 1 / 2,
                     child: card(),
                   ),
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 1/2,
+                    height: MediaQuery.of(context).size.height * 1 / 2,
                     alignment: Alignment.bottomCenter,
                     padding: EdgeInsets.all(20),
                     child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 50,
-                      child: logoutButton(context, '/')),
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: logoutButton(context, '/')),
                   ),
                 ],
               ),
@@ -62,21 +71,20 @@ class HomeScreen extends StatelessWidget {
 }
 
 ElevatedButton logoutButton(BuildContext context, String route) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.pushNamedAndRemoveUntil(context, route, ModalRoute.withName('/'));
-      },
-      style: ElevatedButton.styleFrom(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-          backgroundColor: Color(0xff29B1CC)),
-      child: Text(
-        "Logout",
-        style: GoogleFonts.roboto(
-            textStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Colors.white)),
-      ),
-    );
-  }
+  return ElevatedButton(
+    onPressed: () {
+      Navigator.pushNamedAndRemoveUntil(
+          context, route, ModalRoute.withName('/'));
+    },
+    style: ElevatedButton.styleFrom(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        backgroundColor: Color(0xff29B1CC)),
+    child: Text(
+      "Logout",
+      style: GoogleFonts.roboto(
+          textStyle: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white)),
+    ),
+  );
+}
